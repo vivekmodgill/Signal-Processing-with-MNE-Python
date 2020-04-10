f38f1d3d5ac0f2f93d8565ca251930cd9e9f791b
Vivek Sharma <vivek.sharma1510@gmail.com>
2020-04-11 04:41:55 +0530

    Update functional_connectivity.py

--- functional_connectivity.py
+++ functional_connectivity.py
@@ -6,19 +6,17 @@ from mne.connectivity import spectral_connectivity
 from mne.viz import circular_layout, plot_connectivity_circle
 import os
 
-j = ['CC120120', 'CC120137', 'CC120264', 'CC122016', 'CC220107', 'CC220203', 'CC221336', 'CC222120', 'CC320022', 'CC320059', 'CC320814', 'CC321544', 'CC410119', 'CC410182', 'CC420061', 'CC420286', 'CC420435', 'CC420589', 'CC420776', 'CC510255', 'CC510438', 'CC520239']
+j = ['file_names_without_extension']
 k = '.fif'
-A = '-trans.fif'
-Z = '/rest/transdef_mf2pt2_rest_raw.fif'
 D = '.npy'
 
 for i in j:
-    subjects_dir = '/home/siddharth/Downloads/freesurfer/subjects/'
-    subject = 'collin'
-    bem = os.path.join('/home/siddharth/Downloads/freesurfer/subjects/collin/bem_sol.fif')
-    src = os.path.join('/home/siddharth/Downloads/freesurfer/subjects/collin/src.fif')
-    trans = os.path.join('/home/siddharth/Vivek/Working/Data/fiducials/new_22_fiducials/', i+A)
-    raw = mne.io.read_raw_fif(os.path.join('/home/siddharth/Work/vivek/', i+k), verbose='error')
+    subjects_dir = 'path of the freesurfer subject directory'
+    subject = 'subject name'
+    bem = os.path.join('BEM solution file path')
+    src = os.path.join('src solution file path')
+    trans = os.path.join('trans solution file path')
+    raw = mne.io.read_raw_fif(os.path.join('raw file path', i+k), verbose='error')
     raw.crop(0, None).load_data().pick_types(meg=True, eeg=False).resample(90)
     raw.apply_gradient_compensation(0)
     cov = mne.make_ad_hoc_cov(raw.info, std=None, verbose=None)
@@ -45,18 +43,10 @@ for i in j:
     fmax3 = 30
     sfreq = raw.info['sfreq']
     con0, freqs, times, n_epochs, n_tapers = spectral_connectivity(label_ts, method='wpli2_debiased', mode='multitaper', sfreq=sfreq, fmin=fmin0, fmax=fmax0, faverage=True, mt_adaptive=True, n_jobs=1)
-    np.save(os.path.join('/home/siddharth/Vivek/Working/Data/results/wPLI/delta/new_22_added/', i+D), con0)
+    np.save(os.path.join('path for result files to be saved', i+D), con0)
     con1, freqs, times, n_epochs, n_tapers = spectral_connectivity(label_ts, method='wpli2_debiased', mode='multitaper', sfreq=sfreq, fmin=fmin1, fmax=fmax1, faverage=True, mt_adaptive=True, n_jobs=1)
-    np.save(os.path.join('/home/siddharth/Vivek/Working/Data/results/wPLI/theta/new_22_added/', i+D), con1)
+    np.save(os.path.join('path for result files to be saved', i+D), con1)
     con2, freqs, times, n_epochs, n_tapers = spectral_connectivity(label_ts, method='wpli2_debiased', mode='multitaper', sfreq=sfreq, fmin=fmin2, fmax=fmax2, faverage=True, mt_adaptive=True, n_jobs=1)
-    np.save(os.path.join('/home/siddharth/Vivek/Working/Data/results/wPLI/alpha/new_22_added/', i+D), con2)
+    np.save(os.path.join('path for result files to be saved', i+D), con2)
     con3, freqs, times, n_epochs, n_tapers = spectral_connectivity(label_ts, method='wpli2_debiased', mode='multitaper', sfreq=sfreq, fmin=fmin3, fmax=fmax3, faverage=True, mt_adaptive=True, n_jobs=1)
-    np.save(os.path.join('/home/siddharth/Vivek/Working/Data/results/wPLI/beta/new_22_added/', i+D), con3)
-
-
-
-
-
-
-
-
+    np.save(os.path.join('path for result files to be saved', i+D), con3)
